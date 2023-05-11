5b3bbc25101e41d134d6ea597b49bbeb6745fce3
aaltzi <aaltzibar007@gmail.com>
2023-05-11 22:28:27 +0200

    Update GrepoSTool.js

--- tampermonkey script/GrepoSTool.js
+++ tampermonkey script/GrepoSTool.js
@@ -5,6 +5,9 @@
 // @author       You
 // @description  try to take over the world!
 // @match        https://*.grepolis.com/game/*
+// @updateURL    https://raw.githubusercontent.com/aaltzi/GrepoSTool/main/js/script.js
+// @downloadURL	 https://raw.githubusercontent.com/aaltzi/GrepoSTool/main/js/script.js
+// @require      https://raw.githubusercontent.com/aaltzi/GrepoSTool/main/js/script.js
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_xmlhttpRequest
 // @grant        GM_log
@@ -13,6 +16,7 @@
 
 /*
    Ejemplo a seguir: https://api.grepodata.com/script/indexer.js?v=467657
+   https://api.grepodata.com/script/indexer.css?v=467657
    Página Grepo: https://api.grepodata.com/script/indexer.js?v=467657
 */
 
@@ -21,11 +25,26 @@
     'use strict';
 
     console.log('Inicio Script GrepoSTool');
+    /*  https://raw.githubusercontent.com/aaltzi/GrepoSTool/main/js/script.js  */
+    // Load remote JS OKEY
+    /*
+    GM_xmlhttpRequest({
+        method : "GET",
+        // from other domain than the @match one (.org / .com):
+        url : "https://raw.githubusercontent.com/aaltzi/GrepoSTool/main/js/script.js",
+        onload : (ev) =>
+        {
+            let e = document.createElement('script');
+            e.innerText = ev.responseText;
+            document.head.appendChild(e);
+        }
+    });
+    */
+
 
     var CustomStyleJS = document.createElement('script');
     CustomStyleJS.type = 'text/javascript';
-    /*CustomStyleJS.src = 'https://raw.githubusercontent.com/aaltzi/GrepoSTool/main/js/script.js';*/
-    CustomStyleJS.src = 'http://grepostool.epizy.com/js/script.js';
+    CustomStyleJS.src = 'https://raw.githubusercontent.com/aaltzi/GrepoSTool/main/js/script.js';
     document.getElementsByTagName("head")[0].appendChild(CustomStyleJS);
     /*
     var CustomStyleCSS = document.createElement('link');
